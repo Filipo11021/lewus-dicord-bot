9482105b308c4c1f223e3bf786b69fb9ae8a6d17
filip <filipfilip280@gmail.com>
2021-10-11 22:35:46 +0200

    update

--- bot.js
+++ bot.js
@@ -10,65 +10,58 @@ const client = new Client({
   ],
 })
 
+const messagesArr = [
+  { messageId: '897194808848814151', name: 'T1_vs_EDG' },
+  { messageId: '897194906437697537', name: 'RGE_vs_DWG' },
+]
+
 client.on('ready', async () => {
   const guild = await client.guilds.fetch('734427161695617125')
   const channel = guild.channels.cache.get('896823650727104523')
 
-  const DWGvsFPX = '896840173067857952'
-  const RNGvsPSG = '896840204378316800'
-  const HLEvsFNC = '896840250377244742'
-  const LNGvsGNG = '896840298523668530'
-  const DFMvsT1 = '896840314545918002'
-  const EDGvs100T = '896840346212892722'
-  const TLvsMAD = '896840360771334175'
-  const C9vsRGE = '896840383412203591'
-  const msgArr = [
-    { messageId: DWGvsFPX, name: 'DWG_vs_FPX' },
-    { messageId: RNGvsPSG, name: 'RNG_vs_PSG' },
-    { messageId: HLEvsFNC, name: 'HLE_vs_FNC' },
-    { messageId: LNGvsGNG, name: 'LNG_vs_GNG' },
-    { messageId: DFMvsT1, name: 'DFM_vs_T1' },
-    { messageId: EDGvs100T, name: 'EDG_vs_100T' },
-    { messageId: TLvsMAD, name: 'TL_vs_MAD' },
-    { messageId: C9vsRGE, name: 'C9_vs_RGE' },
-  ]
-
-  msgArr.forEach(async (e) => {
+  messagesArr.forEach(async (e) => {
     const msg = await channel.messages.fetch(e.messageId)
-    const red = await lots_of_messages_getter(msg, '🔴')
-    const blue = await lots_of_messages_getter(msg, '🔵')
+    const blue = await getUsers(msg, '🔵')
+    const red = await getUsers(msg, '🔴')
 
     console.log(e.name, blue.length, red.length)
     const obj = { blue, red }
-    fs.writeFileSync(`./${e.name}.json`, JSON.stringify(obj))
+
+    const path = `./${e.name}.json`
+    fs.closeSync(fs.openSync(path, 'w'))
+    fs.writeFileSync(path, JSON.stringify(obj))
   })
 
-  async function lots_of_messages_getter(message, emoji) {
-    const sum_messages = []
-    let last_id
+  async function getUsers(message, emoji) {
+    const sumUsers = []
+    let lastId
 
     while (true) {
       const options = { limit: 100 }
-      if (last_id) {
-        options.after = last_id
+      if (lastId) {
+        options.after = lastId
       }
 
       const messages = await message.reactions.cache
         .get(emoji)
         .users.fetch(options)
-      sum_messages.push(...Array.from(messages))
-      //console.log(messages.size)
+      sumUsers.push(...Array.from(messages))
+   
       if (messages.size != 100) {
         break
       }
-      last_id = messages.last().id
+      lastId = messages.last().id
     }
 
-    const s = sum_messages.map((e) => e[1].username)
-    //console.log(s.length)
-    let unique = [...new Set(s)]
-    //console.log(unique.length)
-    return unique
+    const usersArr = sumUsers.map((userObj) => {
+      const obj = {
+        username: userObj[1].username,
+        tag: userObj[1].discriminator,
+      }
+      return obj
+    })
+
+    return usersArr
   }
 })
 
