74e902edc0c499aaa87d07eeaa81c3bd76478226
filip <filipfilip280@gmail.com>
2021-10-12 20:53:26 +0200

    update v3

--- bot.js
+++ bot.js
@@ -10,27 +10,79 @@ const client = new Client({
   ],
 })
 
-const messagesArr = [
-  { messageId: '897194808848814151', name: 'T1_vs_EDG' },
-  { messageId: '897194906437697537', name: 'RGE_vs_DWG' },
+//['team1','team2']
+const matches = [
+  ['T1', 'EDG'],
+  ['RGE', 'DWG'],
+  ['PSG', 'HLE'],
+]
+const day = '2'
+
+const resultsCmd = '!results'
+const startCmd = '!start'
+const adminUsers = [
+  { username: 'Hubert', tag: '4251' },
+  { username: 'Lewus', tag: '7210' }
 ]
 
-client.on('ready', async () => {
-  const guild = await client.guilds.fetch('734427161695617125')
-  const channel = guild.channels.cache.get('896823650727104523')
 
-  messagesArr.forEach(async (e) => {
-    const msg = await channel.messages.fetch(e.messageId)
-    const blue = await getUsers(msg, '🔵')
-    const red = await getUsers(msg, '🔴')
+const guildId = '527453668858003476'
+const channelId = '877333930603339797'
 
-    console.log(e.name, blue.length, red.length)
-    const obj = { blue, red }
+client.on('ready', async () => {})
 
-    const path = `./${e.name}.json`
+client.on('messageCreate', async (message) => {
+  const guild = await client.guilds.fetch(guildId)
+  const channel = guild.channels.cache.get(channelId)
+
+  const checkAdmins = adminUsers.some((e) => e.username === message.author.username && e.tag === message.author.discriminator)
+  if (message.content === resultsCmd && checkAdmins) {
+    const res = fs.readFileSync(`./messages.json`, { encoding: 'utf8' })
+    const messages = JSON.parse(res)
+    const currentDate = new Date()
+    const currentTime = currentDate.toLocaleTimeString().replaceAll(':', '.')
+    const currentDay = `day${day}`
+    const dir = `${currentTime} ${currentDay}`
+    fs.mkdirSync(dir)
+
+    for (const e of messages) {
+      const msg = await channel.messages.fetch(e.messageId)
+      const blue = await getUsers(msg, '🔵')
+      const red = await getUsers(msg, '🔴')
+
+      console.log(e.name, blue.length, red.length)
+      const obj = { blue, red }
+
+      const path = `./${dir}/test_${e.name}.json`
+      fs.closeSync(fs.openSync(path, 'w'))
+      fs.writeFileSync(path, JSON.stringify(obj))
+    }
+
+    fs.closeSync(fs.openSync('./currentDir.json', 'w'))
+    fs.writeFileSync('./currentDir.json', JSON.stringify(dir))
+  }
+  if (message.content === startCmd && checkAdmins) {
+    let messages = []
+    const guild = await client.guilds.fetch(guildId)
+    const channel = guild.channels.cache.get(channelId)
+    channel.send(`Worlds 2021 Group Stage Day ${day}`)
+    for (const match of matches) {
+      const message = await channel.send(`${match[0]} vs ${match[1]}`)
+      await message.react('🔵')
+      await message.react('🔴')
+    
+      messages.push({
+        messageId: message.id,
+        name: `${match[0]}_vs_${match[1]}`,
+        blue: match[0],
+        red: match[1],
+      })
+    }
+    const path = './messages.json'
     fs.closeSync(fs.openSync(path, 'w'))
-    fs.writeFileSync(path, JSON.stringify(obj))
-  })
+    fs.writeFileSync(path, JSON.stringify(messages))
+    console.log('end')
+  }
 
   async function getUsers(message, emoji) {
     const sumUsers = []
@@ -46,7 +98,7 @@ client.on('ready', async () => {
         .get(emoji)
         .users.fetch(options)
       sumUsers.push(...Array.from(messages))
-   
+
       if (messages.size != 100) {
         break
       }
